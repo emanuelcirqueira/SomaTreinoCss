110a3edae8675490941332a40f875455461403de
emanuelcirqueira <emanuel.cirqueira12@gmail.com>
2023-09-15 22:36:59 -0300

    Criação somar JS

---
+++ script/script.js
@@ -0,0 +1,14 @@
+
+
+
+
+function somar () {
+    var primeiro = parseInt( document.getElementById("primeiro").value)
+    var segundo = parseInt( document.getElementById("segundo").value)
+
+    var valor = document.getElementById("valor")
+    valor.value = (primeiro+segundo)
+
+
+}
+
